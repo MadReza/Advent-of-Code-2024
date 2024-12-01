b4e2efe492a67d63fdde8488721a01f9eb6b252a
Reza Madabadi <rmadabadi@solutionsmedias360.com>
2024-12-01 12:58:23 -0500

    Day 1: Unzip, groupingBy, fold & require

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,38 @@
+import kotlin.math.abs
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(lines: List<String>): Int {
+        val firstMutableList: MutableList<Int> = mutableListOf()
+        val secondMutableList: MutableList<Int> = mutableListOf()
+        lines.map {
+            val (first, second) = it.split("\\s+".toRegex())
+            firstMutableList.add(first.toInt())
+            secondMutableList.add(second.toInt())
+        }
+        firstMutableList.sort()
+        secondMutableList.sort()
+
+        var sum = 0
+        (0..<firstMutableList.size).forEach {
+            sum += abs(secondMutableList[it] - firstMutableList[it])
+        }
+
+        return sum
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(lines: List<String>): Long {
+        val (left, right) = lines.map {
+            val (firstWord, secondWord) = it.split("""\s+""".toRegex())
+            firstWord.toLong() to secondWord.toLong()
+        }.unzip()
+
+        val frequencies = right.groupingBy { it }.eachCount()
+
+        return left.fold(0L) { acc, num ->
+            acc + num * frequencies.getOrDefault(num, 0)
+        }
+
     }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
